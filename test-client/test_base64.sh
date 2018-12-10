4bcd0a3bd93ed745572cedd30b80399467a9b95b
Mikhail Kantur <kami@slink.ws>
2018-12-10 20:15:58 +1000

    added js and console test clients

---
+++ test-client/test_base64.sh
@@ -0,0 +1,36 @@
+#/usr/bin/env bash
+
+SERVICE_URL=http://localhost:7070/test
+
+
+
+shopt -s nocasematch
+get_mime_type() {
+  case "$1" in
+  "jpg") echo "image/jpeg";;
+  "jpeg") echo "image/jpeg";;
+  "gif") echo "image/gif";;
+  "png") echo "image/png";;
+  *) echo "";;
+  esac
+}  
+jsonify_file() {
+  filename=$(basename -- "$file")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  type=`get_mime_type $extension`
+  bs64=`base64 $file`
+  echo "{\"name\":\"$(basename $file)\",\"type\":\"$type\",\"base64\":\"$bs64\"}" 
+}
+
+JSON="["
+for file in $@; do
+  JSON="$JSON$(jsonify_file "$file"),"
+done
+JSON="${JSON%?}]"
+
+curl \
+     --header "Content-Type: application/json" \
+     --request POST \
+     --data $JSON \
+     $SERVICE_URL
---
+++ test-client/test_multipart.sh
@@ -0,0 +1,14 @@
+#/usr/bin/env bash
+
+SERVICE_URL=http://localhost:7070/test
+
+
+FILES=""
+for file in $@; do
+  FILES="$FILES -F file=@$file"
+done
+
+curl \
+     --request POST \
+     $FILES \
+     $SERVICE_URL
